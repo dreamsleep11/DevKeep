f44c9c1b39b50e00a561a93fff8448b56fcfde4a
Liupeijiang <lele4900@126.com>
2018-02-02 13:12:13 +0800

    更改项目目录

--- Proxy/src/server.js
+++ Proxy/src/server.js
@@ -23,11 +23,14 @@ proxy.on('error', function (err) {
 //   agent: false
 // }
 server = http.createServer(function (req, res) {
+  // console.info('reqreqreqreqreqreqreqreqreqreq')
+  // console.info(req)
+  // console.info('reqreqreqreqreqreqreqreqreqreq')
   // var finalUrl = req.url,
   let finalUrl = 'https://vhsupply.test.viewchain.net'
   let finalAgent = null
   let parsedUrl = url.parse(finalUrl)
-  console.info(req.url)
+  // console.info(req.url)
   if (parsedUrl.protocol === 'https:') {
     finalAgent = https.globalAgent
   } else {
@@ -37,17 +40,30 @@ server = http.createServer(function (req, res) {
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
   let sendUrl = req.url
   console.log('sendUrl：' + sendUrl + '，client ip:' + ip + ', host:' + host)
+  // proxy.on('proxyRes', function (proxyRes, req, res, options) {
+  //   // proxyReq.setHeader('X-Special-Proxy-Header', 'foobar')
+  //   console.log('proxyResproxyResproxyResproxyResproxyRes')
+  //   console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2))
+  //   // console.log('RAW Response from the target', JSON.stringify(proxyRes.body, true, 2))
+  // })
+  // proxy.on('proxyReq', function (proxyRes, req, res, options) {
+  //   // proxyReq.setHeader('X-Special-Proxy-Header', 'foobar')
+  //   if (req.body) {
+  //     console.log('RAW Response from the target', JSON.stringify(req.body, true, 2))
+  //   }
+  //   // console.log('RAW Response from the target', JSON.stringify(proxyRes.body, true, 2))
+  // })
   switch (true) {
     case /^\/api1\/api/.test(sendUrl):
       console.info('/api1/api 拦截1111成功')
       proxy.web(req, res, {
-        target: 'https://www.baidu.com',
+        target: 'https://vhsupply.test.viewchain.net',
         agent: finalAgent,
-        headers: { host: url.parse('https://www.baidu.com').hostname },
+        headers: { host: url.parse('https://vhsupply.test.viewchain.net').hostname },
         prependPath: false,
         xfwd: true,
         hostRewrite: finalUrl.host,
-        protocolRewrite: url.parse('https://www.baidu.com').protocol
+        protocolRewrite: url.parse('https://vhsupply.test.viewchain.net').protocol
       })
       break
     default:
--- src/MINM/server/test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-module.exports = {
-  doTest: function () {
-    return global.co(function * () {
-      let results = yield global.mysql_pool.exec({ sql: 'select * from Api', options: undefined })
-      return results
-    })
-  }
-}
